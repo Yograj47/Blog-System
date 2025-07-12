07180f44063acd53ef5f6d962a2cc82729a4f950
Hemanta Rijal <hemantarijal579@gmail.com>
2025-07-12 13:53:31 +0545

    Updated some content

--- Frontend/Blog-system/src/pages/Blog.jsx
+++ Frontend/Blog-system/src/pages/Blog.jsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/blog.css';
 import BlogCard from '../components/blogCard';
+import Pagination from '../utils/Pagination';
 
 function Blog() {
     const [blogs, setBlogs] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('All');
+    const [currPage, setCurrPage] = useState(2);
+    const [postPerPage, setPostPerPage] = useState(8);
 
     const fetchBlogs = async (category) => {
         const url =
@@ -21,6 +24,10 @@ function Blog() {
         }
     };
 
+    const lastPostIndex = currPage * postPerPage
+    const firstPostIndex = lastPostIndex - postPerPage
+    const currPost = blogs.slice(firstPostIndex, lastPostIndex)
+
     useEffect(() => {
         fetchBlogs(selectedCategory);
     }, [selectedCategory]);
@@ -28,42 +35,45 @@ function Blog() {
     const categories = ['All', 'Technology', 'Health', 'Food', 'Lifestyle', 'Travel', 'Other'];
 
     return (
-        <main className="blog">
-            <div className="blog__main">
-                <div className="blog__filter">
-                    <ul className="blog__filter-list">
-                        {categories.map((cat) => (
-                            <li
-                                key={cat}
-                                className={`blog__filter-item ${selectedCategory === cat ? 'active' : ''}`}
-                                onClick={() => setSelectedCategory(cat)}
-                            >
-                                {cat}
-                            </li>
-                        ))}
-                    </ul>
+        <>
+            <main className="blog">
+                <div className="blog__main">
+                    <div className="blog__filter">
+                        <ul className="blog__filter-list">
+                            {categories.map((cat) => (
+                                <li
+                                    key={cat}
+                                    className={`blog__filter-item ${selectedCategory === cat ? 'active' : ''}`}
+                                    onClick={() => setSelectedCategory(cat)}
+                                >
+                                    {cat}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="blog__grid">
+                        <ul className="blog_page__list" id={selectedCategory}>
+                            {currPost.map((blog) => (
+                                <BlogCard key={blog._id} blog={blog} />
+                            ))}
+                        </ul>
+                    </div>
                 </div>
 
-                <div className="blog__grid">
-                    <ul className="blog_page__list" id={selectedCategory}>
-                        {blogs.map((blog) => (
-                            <BlogCard key={blog._id} blog={blog} />
-                        ))}
-                    </ul>
-                </div>
-            </div>
-
-            <aside className="blog__sidebar">
-                <div className="blog__trending">
-                    <h3 className="blog__sidebar-heading">🔥 Trending Blogs</h3>
-                    {/* Add trending cards later */}
-                </div>
-                <div className="blog__editor-choice">
-                    <h3 className="blog__sidebar-heading">✍️ Editor's Picks</h3>
-                    {/* Add editor's pick cards later */}
-                </div>
-            </aside>
-        </main>
+                <aside className="blog__sidebar">
+                    <div className="blog__trending">
+                        <h3 className="blog__sidebar-heading">🔥 Trending Blogs</h3>
+                        {/* Add trending cards later */}
+                    </div>
+                    <div className="blog__editor-choice">
+                        <h3 className="blog__sidebar-heading">✍️ Editor's Picks</h3>
+                        {/* Add editor's pick cards later */}
+                    </div>
+                </aside>
+                <Pagination totalPost={blogs.length} postPerPage={postPerPage} setCurrPage={setCurrPage} currPage={currPage} />
+            </main>
+        </>
     );
 }
 
